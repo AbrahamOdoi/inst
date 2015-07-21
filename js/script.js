73f54f1777c09473a74364c226cfb4478fea2753
AbrahamOdoi <AbrahamOdoi@users.noreply.github.com>
2015-07-21 11:13:23 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-$(document).on("pagecreate", function() {
-	$("#splahLabel1").slideDown(3000, function() {
-		$("#splahLabel2").fadeIn(3000, function() {
-			window.location = 'login.php';
-		});
-	});
-});
+// $(document).on("pagecreate", function() {
+// 	$("#splahLabel1").slideDown(3000, function() {
+// 		$("#splahLabel2").fadeIn(3000, function() {
+// 			window.location = 'login.php';
+// 		});
+// 	});
+// });
